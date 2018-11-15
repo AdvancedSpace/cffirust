b46018dfbab15ee68f7f9ddb0d18614a1d8b5c6d
Christopher Rabotin <christopher.rabotin@gmail.com>
2018-11-15 15:00:26 -0700

    Added the rust code (but it's buggy)

--- demo/clib.c
+++ demo/clib.c
@@ -1,23 +1,8 @@
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-double norm(const double vector[], const uint vsize) {
-  double val = 0;
-  ushort i;
-  for (i = 0; i < vsize; i++) {
-    val += vector[i] * vector[i];
-  }
-  return sqrt(val);
-}
-
-void subv(const double A[], const double B[], double C[], const uint vsize) {
-  ushort i;
-  for (i = 0; i < vsize; i++) {
-    C[i] = A[i] - B[i];
-  }
-}
+#include "clinalg.h"
 
 int main() {
   time_t t;
--- demo/clinalg.h
+++ demo/clinalg.h
@@ -1,20 +1,19 @@
 #include <math.h>
-#include <stdbool.h>
-#include <stdint.h>
-#include <sys/types.h>
+#include <stdlib.h>
+#include <time.h>
 
-double norm(const double vector[]) {
+double norm(const double vector[], const uint vsize) {
   double val = 0;
   ushort i;
-  for (i = 0; i < 3; i++) {
+  for (i = 0; i < vsize; i++) {
     val += vector[i] * vector[i];
   }
   return sqrt(val);
 }
 
-void subv(const double A[], const double B[], double C[]) {
+void subv(const double A[], const double B[], double C[], const uint vsize) {
   ushort i;
-  for (i = 0; i < 3; i++) {
+  for (i = 0; i < vsize; i++) {
     C[i] = A[i] - B[i];
   }
 }
