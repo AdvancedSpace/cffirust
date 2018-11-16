9250c1b5c3ded91155216b1744945c6b4a235a58
Christopher Rabotin <rabotin@advanced-space.com>
2018-11-16 12:07:40 -0700

    Updated the error hdl code for better representation

--- error_hdl/main.c
+++ error_hdl/main.c
@@ -28,14 +28,13 @@ int main() {
   }
   cur_stor_pos++;
 
-  read_tm2(&read_tm_errno, read_tm_storage, cur_stor_pos++);
+  read_tm2(&read_tm_errno, read_tm_storage, cur_stor_pos);
 
   if (read_tm_errno == NO_ERROR) {
     printf("TM #2: no error -- value = %f\n", read_tm_storage[cur_stor_pos]);
   } else {
     printf("TM #2: errored -- no value to report\n");
   }
-  cur_stor_pos++;
 
   // At this point, nothing prevents a programmer from coding some logic based
   // on the value of telemetry reading, even if it has failed. This seemingly
@@ -43,9 +42,9 @@ int main() {
 
   // For example:
   printf("\nTo recap, all telemetry to be downlinked includes:\n");
-  while (cur_stor_pos > 1) {
+  for (cur_stor_pos = 0; cur_stor_pos<num_tms; cur_stor_pos++) {
     // Will eventually use an uninitialized value.
-    printf("TM #%d = %f\n", cur_stor_pos, read_tm_storage[--cur_stor_pos]);
+    printf("TM #%d = %f\n", cur_stor_pos+1, read_tm_storage[cur_stor_pos]);
   }
 
   return 0;
--- error_hdl/main.rs
+++ error_hdl/main.rs
@@ -15,11 +15,11 @@ fn main() {
     // NOTE: All variables are read-only unless specified otherwise.
     let num_tms = 2;
     // All variables MUST be initialized -- in this case we set it to zero.
-    let mut read_tm_storage = vec![0.0; num_tms];
+    let mut read_tm_storage = Vec::with_capacity(num_tms);
 
     match read_tm1() {
         Ok(tm_value) => {
-            read_tm_storage[0] = tm_value;
+            read_tm_storage.push(tm_value);
             println!("TM #1: no error -- value = {}", tm_value);
         }
         Err(_) => {
@@ -29,7 +29,7 @@ fn main() {
 
     match read_tm2() {
         Ok(tm_value) => {
-            read_tm_storage[1] = tm_value;
+            read_tm_storage.push(tm_value);
             println!("TM #2: no error -- value = {}", tm_value);
         }
         Err(_) => {
@@ -39,6 +39,6 @@ fn main() {
 
     println!("\nTo recap, all telemetry to be downlinked includes:");
     for (pos, val) in read_tm_storage.iter().enumerate() {
-        println!("TM #{} = {}", pos, val);
+        println!("Valid TM #{} = {}", pos + 1, val);
     }
 }
