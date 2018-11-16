2508e7c6ed2c447c479bd2178243625e9cb35bee
Christopher Rabotin <rabotin@advanced-space.com>
2018-11-16 14:02:01 -0700

    Added CPP example

---
+++ dropin_demo/withlib.cpp
@@ -0,0 +1,29 @@
+// #include <cstdlib>
+#include <iostream>
+#include <random>
+// #include <ctime>
+
+#include "clinalg.h"
+
+
+int main() {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> dis(-2.0e-16, 2e16);
+
+  /*
+    Generate 2 vectors of size 3, compute the norm of their difference,
+    repeat 50 million times, print the total of norms, and return.
+   */
+  uint i;
+  double totals = 0; // Avoids optimizing out the loop.
+  for (i = 0; i < 5e7; i++) {
+    double a[] = {dis(gen), dis(gen), dis(gen)};
+    double b[] = {dis(gen), dis(gen), dis(gen)};
+    double o[] = {0, 0, 0};
+    subv(a, b, o, 3);
+    totals += norm(o, 3);
+  }
+  std::cout << totals << std::endl;
+  return 0;
+}
